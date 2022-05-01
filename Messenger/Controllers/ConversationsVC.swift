bc2cdcadaea5a34388b4bd98ea6b37f0f300d4d6
System Administrator <root@zhangjunjiedeMacBook-Air.local>
2022-05-01 20:56:50 +0800

    Wrap up getting Started

--- Messenger/Controllers/ConversationsVC.swift
+++ Messenger/Controllers/ConversationsVC.swift
@@ -1,19 +1,16 @@
 //
-//  ViewController.swift
+//  ConversationsVC.swift
 //  Messenger
 //
 //  Created by 張竣傑 on 2022/5/1.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class ConversationsVC : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ Messenger/Controllers/ProfileVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileVC.swift
+//  Messenger
+//
+//  Created by 張竣傑 on 2022/5/1.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
