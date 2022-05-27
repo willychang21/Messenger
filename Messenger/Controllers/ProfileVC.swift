7086bd21fb142af5308dd842086d6e7c39474d87
張竣傑 <zhangjunjie@zhangjunjiedeMacBook-Air.local>
2022-05-27 16:43:05 +0800

    Update ProfileVCand Readme

--- Messenger/Controllers/ProfileVC.swift
+++ Messenger/Controllers/ProfileVC.swift
@@ -2,15 +2,76 @@ import UIKit
 import FirebaseAuth
 import FacebookLogin
 import GoogleSignIn
+import SDWebImage
+
+enum ProfileViewModelType {
+    case info, logout
+}
+
+struct ProfileViewModel {
+    let viewModelType: ProfileViewModelType
+    let title: String
+    let handler: (() -> Void)?
+}
 
 class ProfileVC: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
-    let data = ["Log out"]
+    var data = [ProfileViewModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(ProfileTableViewCell.self,
+                           forCellReuseIdentifier: ProfileTableViewCell.identifier)
+        data.append(ProfileViewModel(viewModelType: .info,
+                                     title: "Name: \(UserDefaults.standard.value(forKey: "name") as? String ?? "No Name")",
+                                     handler: nil))
+        data.append(ProfileViewModel(viewModelType: .info,
+                                     title: "Email: \(UserDefaults.standard.value(forKey: "email") as? String ?? "No Email")",
+                                     handler: nil))
+        data.append(ProfileViewModel(viewModelType: .logout,
+                                     title: "Log Out",
+                                     handler: { [weak self] in
+            guard let strongSelf = self else {
+                return
+            }
+            let actionSheet = UIAlertController(title: "Are you sure you want to log out?",
+                                                message: "",
+                                                preferredStyle: .actionSheet)
+            actionSheet.addAction(UIAlertAction(title: "Log out",
+                                                style: .destructive,
+                                                handler: { [weak self] _ in
+                guard let strongSelf = self else {
+                    return
+                }
+                
+                // Facebook - Log Out
+                FacebookLogin.LoginManager().logOut()
+                
+                // Google - Log out
+                GIDSignIn.sharedInstance.signOut()
+                
+                do {
+                    try FirebaseAuth.Auth.auth().signOut()
+                    
+                    let vc = LoginVC()
+                    let nav = UINavigationController(rootViewController: vc)
+                    nav.modalPresentationStyle = .fullScreen
+                    strongSelf.present(nav, animated: false)
+                } catch {
+                    print("Failed to log out: \(error)")
+                }
+                
+            }))
+            
+            actionSheet.addAction(UIAlertAction(title: "Cancel",
+                                                style: .cancel,
+                                                handler: nil))
+            
+            strongSelf.present(actionSheet, animated: true)
+        }))
+        
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.delegate = self
         tableView.dataSource = self
@@ -44,11 +105,12 @@ class ProfileVC: UIViewController {
         imageView.layer.cornerRadius = imageView.width/2
         headerView.addSubview(imageView)
         
-        StorageManager.shared.downloadURL(for: path) { [weak self] result in
+        StorageManager.shared.downloadURL(for: path) { result in
             
             switch result {
             case .success(let url):
-                self?.downloadImage(imageView: imageView, url: url)
+                imageView.sd_setImage(with: url, completed: nil)
+                //                self?.downloadImage(imageView: imageView, url: url)
             case .failure(let error):
                 print("Failed to get download url: \(error)")
             }
@@ -57,18 +119,18 @@ class ProfileVC: UIViewController {
         return headerView
     }
     
-    func downloadImage(imageView: UIImageView, url: URL) {
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            guard let data = data, error == nil else {
-                return
-            }
-            
-            DispatchQueue.main.async {
-                let image = UIImage(data: data)
-                imageView.image = image
-            }
-        }.resume()
-    }
+    //    func downloadImage(imageView: UIImageView, url: URL) {
+    //        URLSession.shared.dataTask(with: url) { data, _, error in
+    //            guard let data = data, error == nil else {
+    //                return
+    //            }
+    //
+    //            DispatchQueue.main.async {
+    //                let image = UIImage(data: data)
+    //                imageView.image = image
+    //            }
+    //        }.resume()
+    //    }
     
 }
 
@@ -78,51 +140,36 @@ extension ProfileVC: UITableViewDelegate,UITableViewDataSource {
         return data.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
-        cell.textLabel?.textAlignment = .center
-        cell.textLabel?.textColor = .red
+        let viewModel = data[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProfileTableViewCell.identifier, for: indexPath) as! ProfileTableViewCell
+        cell.setUp(with: viewModel)
+//        cell.textLabel?.text = data[indexPath.row]
+//        cell.textLabel?.textAlignment = .center
+//        cell.textLabel?.textColor = .red
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        
-        let actionSheet = UIAlertController(title: "Are you sure you want to log out?",
-                                            message: "",
-                                            preferredStyle: .actionSheet)
-        actionSheet.addAction(UIAlertAction(title: "Log out",
-                                            style: .destructive,
-                                            handler: { [weak self] _ in
-            guard let strongSelf = self else {
-                return
-            }
-            
-            // Facebook - Log Out
-            FacebookLogin.LoginManager().logOut()
-            
-            // Google - Log out
-            GIDSignIn.sharedInstance.signOut()
-            
-            do {
-                try FirebaseAuth.Auth.auth().signOut()
-                
-                let vc = LoginVC()
-                let nav = UINavigationController(rootViewController: vc)
-                nav.modalPresentationStyle = .fullScreen
-                strongSelf.present(nav, animated: false)
-            } catch {
-                print("Failed to log out: \(error)")
-            }
-            
-        }))
-        
-        actionSheet.addAction(UIAlertAction(title: "Cancel",
-                                            style: .cancel,
-                                            handler: nil))
-        
-        present(actionSheet, animated: true)
-        
+        data[indexPath.row].handler?()
     }
     
+}
+
+class ProfileTableViewCell: UITableViewCell {
+    
+    static let identifier = "ProfileTableViewCell"
+    
+    public func setUp(with viewModel: ProfileViewModel) {
+        self.textLabel?.text = viewModel.title
+        
+        switch viewModel.viewModelType {
+        case .info:
+            self.textLabel?.textAlignment = .left
+            self.selectionStyle = .none 
+        case .logout:
+            self.textLabel?.textColor = .red
+            self.textLabel?.textAlignment = .center
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Messenger Real Time Chat App
 A beginners swift project to create a real time chat application in Swift 5 using Firebase.
+## Development Tool Version (Using SPM not CocoaPods)
+* Xcode: 13.3 (13E113)
+* Firebas: 8.15.0
+* GoogleSignIn: 6.1.0
+* Facebook: 13.2.0
+* MessageKit: 3.8.0
+* SDWebImage: 5.12.0
+* Realm: 10.25.2
+* JGProgressHUB: 2.2.0
+
 ## Features
 * Facebook Log In
 * Google Sign In
